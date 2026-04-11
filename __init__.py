e4464848e718e9ad1f88b949a35b090d1d1c3e6d
Mohamed Elwardi <162064010+simomorphine@users.noreply.github.com>
2026-04-11 14:08:56 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2026 
+# Mohamed Elwardi
+# This program is free software: you can redistribute it and/or 
+# modify it under the terms of GNU General Public License
+# by the Free Software Foundation, either version 3 of
+# the License. This program is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
 __author__ = "Mohamed Elwardi"
 __title__ = "Cost Geometry Under Investigation"
 __description__ = "Initial research into the geometric structure of cost within Humble Systems Theory."
