ba455ba4f7cb571d086fc56d19e739da7b1671b2
Mohamed Elwardi <162064010+simomorphine@users.noreply.github.com>
2026-04-11 14:00:45 +0200

    Update complex_dqn.py

--- agents/complex_dqn.py
+++ agents/complex_dqn.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2026 
+# Mohamed Elwardi
+# This program is free software: you can redistribute it and/or 
+# modify it under the terms of GNU General Public License
+# by the Free Software Foundation, either version 3 of
+# the License. This program is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+
 """
 Complex Q-Network agent (HST framework).
 
